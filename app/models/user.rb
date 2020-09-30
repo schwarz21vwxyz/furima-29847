70e94b75c2c4aabf9a9c87094682ad4c51a2cba2
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-09-30 18:45:53 +0900

    バリデーションでエラーメッセージを表示させる実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   validates :nick_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :family_name, presence: true
-  validates :name, presence: true
-  validates :family_name_show, presence: true
-  validates :name_show, presence: true
   validates :birthday, presence: true
 
+  
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（漢字・ひらがな）で入力"}
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（漢字・ひらがな）で入力"}
+    validates :family_name_show, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（カタカナ）で入力"}
+    validates :name_show, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（カタカナ）で入力"}
+    validates :password, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力"}
+  end
 end
