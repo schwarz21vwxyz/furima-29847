74cbfb9589c7e82b0c977860134d98bf53057c05
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-01 18:27:44 +0900

    ADD user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'User' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+  
+    it "nick_nameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nick_name can't be blank")
+    end
+
+    it "emailが空では登録できない" do
+    end
+    it "emailに＠を含んでないと登録できない" do
+    end
+    it "passwardが空だと登録できない" do
+    end
+    it "passwardが６文字以上でないと登録できない" do
+    end
+    it "passwardが半角英数字混合でないと登録できない" do
+    end
+    it "family_nameが空だと登録できない" do
+    end
+    it "nameが空だと登録できない" do
+    end
+    it "family_nameが全角日本語でないと保存できないこと" do
+    end
+    it "nameが全角日本語でないと保存できないこと" do
+    end
+
+  end
+end
