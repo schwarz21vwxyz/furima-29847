0cc69ea267e6264ec1110dcbb9f8550e4950bbc7
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-04 17:28:06 +0900

    Revert "users.controller.rbの修正"
    This reverts commit 843ab3dc76bfa32448a483bf2b16e34dcab9034c.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,7 @@ class UsersController < ApplicationController
     @users = User.new(user_params)
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
-  end
+private
+def user_params
+  params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
 end
