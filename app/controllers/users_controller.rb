becb5b394c9e54f26ee5cdf39bcc1b8fd5e59c02
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-04 18:15:00 +0900

    コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-  def new
-    @users = User.new
-  end
+#   def new
+#     @users = User.new
+#   end
 
 
-  def create
-    @users = User.new(user_params)
-  end
+#   def create
+#     @users = User.new(user_params)
+#   end
 
-private
-def user_params
-  params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
-end
+#   private
+#   def user_params
+#     params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
+#   end
+
+# end
