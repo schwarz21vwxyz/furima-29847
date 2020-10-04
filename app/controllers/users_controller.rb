7bb624977ceaa4a513082f98923e5af44262ecd6
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-04 18:39:36 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,2 @@
-# class UsersController < ApplicationController
-
-#   def new
-#     @users = User.new
-#   end
-
-
-#   def create
-#     @users = User.new(user_params)
-#   end
-
-#   private
-#   def user_params
-#     params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
-#   end
-
-# end
+class UsersController < ApplicationController
+end
