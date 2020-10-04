843ab3dc76bfa32448a483bf2b16e34dcab9034c
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-04 17:25:35 +0900

    users.controller.rbの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     @users = User.new(user_params)
   end
 
-private
-def user_params
-  params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
+  private
+  def user_params
+    params.require(:user).permit(:nick_name, :email, :password, :family_name, :name, :family_name_show, :name_show, :birthday)
+  end
 end
