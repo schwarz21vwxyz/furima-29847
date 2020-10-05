9080e6897eea39b24f81ac16384121a2aa5f549b
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-05 17:20:23 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :show
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :area
+  belongs_to_active_hash :days
 end
