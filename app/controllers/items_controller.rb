18dd2eaf5880cd38ba3454cda039bf07700fa07e
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-05 23:32:59 +0900

    コントローラにcreateアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:comment,:category_id,:show_id,:delivery_id,:area_id,:day_id,:price,:user_id)
+  end
 end
