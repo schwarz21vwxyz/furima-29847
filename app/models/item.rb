bb0b9f2f12f0a27928efc94f3b55a54ecd072f29
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-07 16:08:40 +0900

    バリデーション再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :name, :comment, :price, :user, presence: true
+  validates :image, :name, :comment, :user, presence: true
 
-  validates :category_id, :show_id, :delivery_id, :delivery_id, numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Half-width number"}
+  end
 
-  validates :area_id, numericality: { other_than: 0 } 
+  validates_inclusion_of :price, in: 300..9999999, message: "Out of setting range"
 
+  validates :category_id, numericality: { other_than: 1, message: "Select" } 
+  validates :show_id, numericality: { other_than: 1 , message: "Select"} 
+  validates :delivery_id, numericality: { other_than: 1 , message: "Select"} 
+  validates :area_id, numericality: { other_than: 0 , message: "Select"} 
+  validates :day_id, numericality: { other_than: 1 , message: "Select"} 
 end
