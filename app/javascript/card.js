5d121eb7d48df5c94f7fd1464782afdf817ad79c
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-12 16:19:40 +0900

    受け取ったクレジット情報を削除する記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,9 +21,16 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       }
+
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
-}
+};
+
 window.addEventListener("load", pay);
