d4936bf3012aa756a6d6d7a5815d3a7de461a775
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-14 14:00:39 +0900

    バリデーション設定

--- app/forms/buy.rb
+++ app/forms/buy.rb
@@ -4,13 +4,17 @@ class Buy
   attr_accessor :token, :price, :prefecture_id, :postal_code, :city, :house_number, :building_name,:phone_number, :order_id, :user_id, :item_id
 
 
-  # with_options presence: true do
-  #   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  # end
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :phone_number, numericality: { with: /\A\d{11}\z/, message: "Input only number"}
+  end
+
+  validates :city, :house_number, presence: true
+  validates :prefecture_id, numericality: { other_than: 0 , message: "Select"} 
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id,)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
-
   end
 end
