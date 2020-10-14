d3a01d8019a6a01bb4f3c897dca734edcfae4d0b
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-14 09:52:18 +0900

    コントローラでトークンと購入情報の受け渡し

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,35 @@
 class OrdersController < ApplicationController
   
   def index
-    @order = Item.find(params[:item_id])
+    @order = Buy.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = Buy.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private    
+
+  def order_params
+    params.require(:buy).permit(:price,:postal_code,:city,:house_number,:building_name,:phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+
+  def pay_item
+    Payjp.api_key = "sk_test_9a678f79dbf6abbfeac403b0"
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
