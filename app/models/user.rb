d185dc4a86377057316c9c1cad874cbfa98cfa3a
schwarz21vwxyz <schwarzvwxyz@gmail.com>
2020-10-15 16:49:31 +0900

    新規登録のテストコード
    エラーが出たので修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ApplicationRecord
 
   
   with_options presence: true do
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（漢字・ひらがな）で入力"}
-    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角（漢字・ひらがな）で入力"}
-    validates :family_name_show, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）で入力"}
-    validates :name_show, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）で入力"}
-    validates :password, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力"}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :family_name_show, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :name_show, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i, message: "Include both letters and numbers"}
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
 
   factory :user do
     nick_name { 'furima' }
-    email { 'sample@gmail.com' }
+    email {Faker::Internet.free_email}
     password { 'aaaa1111' }
-    encrypted_password { 'aaaa1111' }
+    # encrypted_password { 'aaaa1111' }
     family_name { '山田' }
     name { '太郎' }
     family_name_show { 'ヤマダ' }
@@ -12,4 +12,4 @@ FactoryBot.define do
     birthday { '2000-12-12' }
 
   end
-end
+end 
